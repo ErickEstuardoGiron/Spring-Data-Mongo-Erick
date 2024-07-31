8de208766871bfe9092ad3bda0b232acde557578
ErickEstuardoGiron <ekarlluvi123@gmail.com>
2024-07-31 08:47:02 -0600

    Erick MongoDB

---
+++ src/main/java/com/erick/mongod/Student.java
@@ -0,0 +1,36 @@
+package com.erick.mongod;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "students")
+public class Student {
+    @Id
+    private String id;
+    private String name;
+    private int edad;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
---
+++ src/test/java/com/erick/mongod/MongodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erick.mongod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
